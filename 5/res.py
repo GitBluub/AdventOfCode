aa9a94c678a1126541d67042fe260cf39e06db6f
Bluub <louis.auzuret@gmail.com>
2021-12-05 12:34:53 +0100

    fiest star

---
+++ 5/res.py
@@ -0,0 +1,28 @@
+
+with open('input') as f:
+    read_data = f.read().splitlines()
+
+vents = []
+n=0
+for i in read_data:
+	a,b = i.split(' -> ')
+	x1, y1 = list(map(int, a.split(",")))
+	x2, y2 = list(map(int, b.split(",")))
+	n=max(n, x1, x2, y1, y2)
+	vents += [([x1, y1], [x2, y2])]
+board = [[0 for i in range(n+1)] for j in range(n+1)]
+
+
+for a, b in vents:
+	x1, y1 = a
+	x2, y2 = b
+	if x1 == x2:
+		y1, y2 = min(y1, y2), max(y1, y2)
+		for i in range(y1, y2+1):
+			board[x1][i] += 1
+	elif y1 == y2:
+		x1, x2 = min(x1, x2), max(x1, x2)
+		for i in range(x1, x2+1):
+			board[i][y1] += 1
+r = sum(sum([1 for x in i if x >= 2]) for i in board)
+print(r)
