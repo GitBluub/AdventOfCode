9405e631c2056699d37a7e86386a1b89e3e4a356
Bluub <louis.auzuret@gmail.com>
2021-12-07 10:03:35 +0100

    second star

--- 7/res.py
+++ 7/res.py
@@ -2,4 +2,4 @@ with open('input') as f:
     read_data = f.read().splitlines()
 
 nbs = list(map(int, read_data[0].split(',')))
-print(min([sum([abs(j-i) for j in nbs]) for i in nbs]))
+print(min([sum([sum(range(1, abs(j-i) + 1)) for j in nbs]) for i in nbs]))
