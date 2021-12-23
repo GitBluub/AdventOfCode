0d773fc9aa378b1f1e9236fdc72592b445060075
Bluub <louis.auzuret@gmail.com>
2021-12-23 12:34:57 +0100

    second star

--- 9/res.py
+++ 9/res.py
@@ -13,5 +13,32 @@ for i, a in enumerate(m):
 			continue
 		if i != len(m) - 1 and m[i + 1][j] <= b:
 			continue
-		lowP += [b + 1]
-print(sum(lowP))
+		lowP += [(i, j)]
+
+basin = []	
+def recBasin(i, j, b):
+	global basin
+	if i * len(m) + j in basin:
+		return
+	if m[i][j] >= b and m[i][j] != 9:
+		basin += [(i * len(m)) + j]
+	else:
+		return
+	if i != 0:
+		recBasin(i-1, j, b)	
+	if i != len(m) - 1:
+		recBasin(i+1, j, b)	
+	if j != 0:
+		recBasin(i, j-1, b)	
+	if j != len(m[0]) - 1:
+		recBasin(i, j+1, b)
+
+def getBasinSize(i, j):
+	global basin
+	basin = []
+	recBasin(i, j, m[i][j])
+	return len(basin)
+
+basinsSize = [getBasinSize(i, j) for i, j in lowP]
+basinsSize.sort()
+print(basinsSize[-1] * basinsSize[-2] * basinsSize[-3])
