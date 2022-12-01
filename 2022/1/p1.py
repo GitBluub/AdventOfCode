590c54e030256ab1bea6ba8a83321c1303c6c4a2
GitBluub <louis.auzuret@gmail.com>
2022-12-01 18:32:27 +0900

    2022 day 1

---
+++ 2022/1/p1.py
@@ -0,0 +1,12 @@
+with open('input') as f:
+    read_data = f.read().splitlines()
+
+i = 0
+a = []
+for l in read_data:
+    if l == "":
+        i += 1
+    else:
+        if len(a) < i + 1: a += [0]
+        a[i] += int(l)
+print(max(a))
