00d66ed3c5e985c2936b1d0fea1b77138e555989
GitBluub <louis.auzuret@gmail.com>
2022-12-06 21:12:47 +0900

    2020

--- 2021/7/res.py
+++
@@ -1,5 +0,0 @@
-with open('input') as f:
-    read_data = f.read().splitlines()
-
-nbs = list(map(int, read_data[0].split(',')))
-print(min([sum([sum(range(1, abs(j-i) + 1)) for j in nbs]) for i in nbs]))
